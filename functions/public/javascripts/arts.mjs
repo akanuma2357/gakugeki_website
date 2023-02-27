b934e3ae0a1117e442ef38e29e63ea8f03c7bbcf
otomamayikuy <103938277+yukiyamamoto0804@users.noreply.github.com>
2023-02-27 17:21:41 +0900

    もっと見るの動作の変更

--- functions/public/javascripts/arts.mjs
+++ functions/public/javascripts/arts.mjs
@@ -1,20 +1,50 @@
 import {urlRequest, downloadFile} from "./firebase.mjs";
 
-const lookMore = async () => {
+const lookMore = async (numList) => {
   const moreButton = document.getElementById("Arts__more");
+  let clickCount = 1
   moreButton.addEventListener("click", async function() {
-    const urls = await urlRequest();
-    const fileURLs = await downloadFile(urls);
-    const imgArea = document.getElementById("Arts__contents");
-    for (const fileURL of fileURLs) {
-      const linkElement = document.createElement("a");
-      linkElement.setAttribute("href", fileURL);
-      imgArea.appendChild(linkElement);
-      const imgElement = document.createElement("img");
-      imgElement.setAttribute("src", fileURL);
-      linkElement.appendChild(imgElement);
+    if (clickCount < 37){
+      const urls = []
+      for (let i = 0; i < 3; i++){
+        const strNum = ("000"+String(numList[i+3*clickCount])).slice(-3)
+        urls.push("AI_arts/"+strNum+".png")
+      }
+      const fileURLs = await downloadFile(urls);
+      const imgArea = document.getElementById("Arts__contents__item");
+      for (const fileURL of fileURLs) {
+        const linkElement = document.createElement("a");
+        linkElement.setAttribute("href", fileURL);
+        imgArea.appendChild(linkElement);
+        const imgElement = document.createElement("img");
+        imgElement.setAttribute("src", fileURL);
+        linkElement.appendChild(imgElement);
+      }
+      clickCount += 1
     }
   }, false);
 };
 
-export {lookMore};
+const getNumList = async () => {
+  const numList = await urlRequest();
+  return numList
+}
+const defaultImage = async (numList) => {
+  const urls = []
+  for (let i = 0; i < 3; i++){
+    const strNum = ("000"+String(numList[i])).slice(-3)
+    urls.push("AI_arts/"+strNum+".png")
+  }
+  const fileURLs = await downloadFile(urls);
+  const imgArea = document.getElementById("Arts__contents__item");
+  for (const fileURL of fileURLs) {
+    const linkElement = document.createElement("a");
+    linkElement.setAttribute("href", fileURL);
+    linkElement.download = "AIart.png"
+    imgArea.appendChild(linkElement);
+    const imgElement = document.createElement("img");
+    imgElement.setAttribute("src", fileURL);
+    linkElement.appendChild(imgElement);
+  }
+}
+export {lookMore,getNumList,defaultImage};
